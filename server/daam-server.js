c014f99c25e201e7f0ac0f579145a268edb3eff9
rapPayne <bob@creator.net>
2018-09-24 08:31:59 -0500

    Prepping for React Native

--- server/daam-server.js
+++ server/daam-server.js
@@ -50,16 +50,8 @@ app.use('/api/theaters', theatersRouter);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
