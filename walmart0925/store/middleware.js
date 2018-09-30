ca50da77feca0396270bc91f31c8ac8dda9ab99b
rapPayne <bob@creator.net>
2018-09-30 17:57:57 -0500

    Removed old files

--- walmart0925/store/middleware.js
+++
@@ -1,5 +0,0 @@
-export const loggingMiddleware = ({getState, dispatch}) => next => action => {
-  console.log(action.type, getState())
-  next(action);
-  console.warn(action.type, getState())
-}
--- walmart0925/store/store.js
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import { reducer } from './reducers';
-import { loggingMiddleware } from './middleware';
-import * as user from '../user.json';
-
-const enhancers = applyMiddleware(loggingMiddleware);
-
-const initialState = { person: user };
-export const store = createStore(reducer, initialState, enhancers);
