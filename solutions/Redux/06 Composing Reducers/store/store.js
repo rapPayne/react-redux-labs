5265c1e33cf4d4b14c29b05fc37d768cc21946d2
rapPayne <bob@creator.net>
2019-08-22 14:47:01 -0500

    Reorganized solutions

--- solutions/Redux/06 Composing Reducers/store/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from 'redux';
-import * as user from '../user.json';
-import reducer from './reducers';
-
-const initialState = { person: user };
-
-export const store = createStore(reducer, initialState);
--- solutions/Redux/07 Middleware/store/middleware.js
+++
@@ -1,7 +0,0 @@
-
-export const saveToLocalStorageMiddleware = ({ getState, dispatch }) => next => action => {
-  next(action);
-  const { first, last } = getState().person.name;
-  localStorage["name"] = `${first} ${last}`;
-  localStorage["lastVisit"] = new Date();;
-};
