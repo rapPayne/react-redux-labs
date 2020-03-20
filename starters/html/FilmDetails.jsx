18057e7412bc06df4b184a084fa240b121f25f37
rapPayne <bob@creator.net>
2020-03-20 17:06:04 -0500

    Added starters

---
+++ starters/html/FilmDetails.jsx
@@ -0,0 +1,23 @@
+<>
+  <div style={{ ...styles.container }} className='mdl-card mdl-shadow--2dp'>
+    <div style={{}}>
+      <h1>Movie title goes here</h1>
+    </div>
+    <div style={{ display: 'flex' }}>
+      <div style={{ flex: '1 1 30%' }}>
+        <img src="POSTER_HERE" alt="" style={styles.poster} />
+      </div>
+      <div style={{ flex: '1 1 70%' }}>
+        <h1>Movie title goes here</h1>
+        <h2>Movie tagline goes here</h2>
+        <p>Movie overview goes here</p>
+        <p>Viewer's ratings: <span>Vote_average</span> / <span>Vote_count</span></p>
+        <p>Released: Release date here</p>
+        <p>Runtime minutes</p>
+        <a href="HOMEPAGE" target="movie_site">Movie's home page here</a>
+        Pick dates will go here
+        Showing times will go here
+      </div>
+    </div>
+  </div>
+</>
