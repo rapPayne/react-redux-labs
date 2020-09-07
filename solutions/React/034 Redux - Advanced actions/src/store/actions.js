3aad81e1b605df35e469375452a48382ae05668c
rapPayne <bob@creator.net>
2020-09-07 08:06:40 -0500

    Added solution for Advanced actions lab

---
+++ solutions/React/034 Redux - Advanced actions/src/store/actions.js
@@ -0,0 +1,30 @@
+export const actionTypes = {
+  SET_CURRENT_DATE: "SET_CURRENT_DATE",
+  SET_CURRENT_FILM: "SET_CURRENT_FILM",
+  SET_CURRENT_SHOWING: "SET_CURRENT_SHOWING",
+  SET_FILMS: "SET_FILMS",
+  SET_TABLES: "SET_TABLES",
+  SET_THEATERS: "SET_THEATERS",
+  SET_SEATS: "SET_SEATS",
+  SET_USER: "SET_USER",
+};
+
+const setCurrentDate = date => ({type: actionTypes.SET_CURRENT_DATE, date});
+const setCurrentFilm = film => ({type: actionTypes.SET_CURRENT_FILM, film});
+const setCurrentShowing = showing => ({type: actionTypes.SET_CURRENT_SHOWING, showing});
+const setFilms = films => ({type: actionTypes.SET_FILMS, films});
+const setTables = tables => ({type: actionTypes.SET_TABLES, tables});
+const setTheaters = theaters => ({type: actionTypes.SET_THEATERS, theaters});
+const setSeats = seats => ({type: actionTypes.SET_SEATS, seats});
+const setUser = user => ({type: actionTypes.SET_USER, user});
+
+export const actions = {
+  setCurrentDate,
+  setCurrentFilm,
+  setCurrentShowing,
+  setFilms,
+  setTables,
+  setTheaters,
+  setSeats,
+  setUser,
+};
