a2f079a731ee9f5b5fc6e9cbe08f2f1807eafa8d
rapPayne <bob@creator.net>
2020-11-26 18:11:47 -0600

    Refactored to handle Android fetches

--- solutions/React Native/021_single_value_components/store/middleware.js
+++ solutions/React Native/021_single_value_components/store/middleware.js
@@ -1,9 +1,12 @@
+import { host } from './helpers';
+
 const fetchFilmsMiddleware = ({dispatch, getState}) => next => action => {
   if (action?.type === "FETCH_FILMS") {
-    fetch("http://localhost:3007/api/films")
+    fetch(`${host}/api/films`)
     .then(res => res.json())
     .then(films => films.forEach(film => dispatch({type:"ADD_FILM",film})))
-    .then(console.log);
+    .catch(err => console.error("Couldn't fetch films", err))
+
   }
   next(action);
 }
