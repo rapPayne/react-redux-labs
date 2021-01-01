b6a2cb9203e941cf481491a1487baec30f672a37
rapPayne <bob@creator.net>
2021-01-01 11:02:26 -0600

    Merged Andrew's version with my original

--- starters/helpers/api_host_maker.js
+++ starters/helpers/api_host_maker.js
@@ -7,11 +7,14 @@ const port = "3007";
 
 export const host = Platform.select({
   ios: `http://localhost:${port}`, 
- android: `http://10.0.2.2::${port}`,
+  android: `http://10.0.2.2:${port}`,
 });
 //`http://<Your actual IP address>:${port}`  Should always work. But doesn't.
 //`http://localhost:${port}`  Works on iOS simulators but fails on Android emulator
-//`http://10.0.2.2::${port}`   Works on Android emulator via Android Studio
+//`http://10.0.2.2:${port}`   Works on Android emulator via Android Studio
 //`http://10.0.3.2:${port}`   Works on Android emulator via Genymotion
 
 //A tethered device will need to use your laptop's IP address
+
+//To find your actual IP address, run the `ipconfig` command on Windows or the
+// `ifconfig` command on a Mac or Linux.
