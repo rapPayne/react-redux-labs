c1c7f65f63ba0ab47cad1fbccb12c46006e285d7
rapPayne <bob@creator.net>
2021-01-11 19:03:14 -0600

    Cleaned out old labs

--- solutions/ReactNative/040_layout_components/Showings.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-export const Showings = ({showings, selected_date}) =>
-showings.map(showing => <Text key={showing.id}>
- {showing.showing_time}
-</Text>)
