464a2a0faf19192c44b887956417b5fa52d1c934
rapPayne <bob@creator.net>
2021-05-30 12:09:07 -0500

    Refreshed lab 034

--- solutions/React/Redux/034 Redux - Advanced actions/src/store/actions.js
+++ solutions/React/Redux/034 Redux - Advanced actions/src/store/actions.js
@@ -3,31 +3,31 @@ export const actionTypes = {
   SET_CURRENT_FILM: "SET_CURRENT_FILM",
   SET_CURRENT_SHOWING: "SET_CURRENT_SHOWING",
   SET_FILMS: "SET_FILMS",
-  SET_SHOWINGS: "SET_SHOWINGS",
   SET_TABLES: "SET_TABLES",
   SET_THEATERS: "SET_THEATERS",
   SET_SEATS: "SET_SEATS",
+  SET_SHOWINGS: "SET_SHOWINGS",
   SET_USER: "SET_USER",
+
 };
 
-const setCurrentDate = date => ({type: actionTypes.SET_CURRENT_DATE, date});
-const setCurrentFilm = film => ({type: actionTypes.SET_CURRENT_FILM, film});
-const setCurrentShowing = showing => ({type: actionTypes.SET_CURRENT_SHOWING, showing});
-const setFilms = films => ({type: actionTypes.SET_FILMS, films});
-const setShowings = showings => ({type: actionTypes.SET_SHOWINGS, showings});
-const setTables = tables => ({type: actionTypes.SET_TABLES, tables});
-const setTheaters = theaters => ({type: actionTypes.SET_THEATERS, theaters});
-const setSeats = seats => ({type: actionTypes.SET_SEATS, seats});
-const setUser = user => ({type: actionTypes.SET_USER, user});
-
+const setCurrentDate = date => ({ type: actionTypes.SET_CURRENT_DATE, date });
+const setCurrentFilm = film => ({ type: actionTypes.SET_CURRENT_FILM, film });
+const setCurrentShowing = showing => ({ type: actionTypes.SET_CURRENT_SHOWING, showing });
+const setFilms = films => ({ type: actionTypes.SET_FILMS, films });
+const setTables = tables => ({ type: actionTypes.SET_TABLES, tables });
+const setTheaters = theaters => ({ type: actionTypes.SET_THEATERS, theaters });
+const setSeats = seats => ({ type: actionTypes.SET_SEATS, seats });
+const setShowings = showings => ({ type: actionTypes.SET_SHOWINGS, showings });
+const setUser = user => ({ type: actionTypes.SET_USER, user });
 export const actions = {
   setCurrentDate,
   setCurrentFilm,
   setCurrentShowing,
   setFilms,
-  setShowings,
   setTables,
   setTheaters,
   setSeats,
+  setShowings,
   setUser,
 };
