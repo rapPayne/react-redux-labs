9e8f13ba05fd8d398036ba3c0f1c48f859fafd0e
rapPayne <bob@creator.net>
2022-03-13 18:57:38 -0500

    Added lab 034

---
+++ solutions/React with Redux/034 Actions and Reducers 101/src/store/reducers.js
@@ -0,0 +1,25 @@
+export const reducer = (state, action) => {
+  console.log("In reducer", state, action);
+  switch (action?.type) {
+    case "SET_CURRENT_DATE":
+      return { ...state, currentDate: action.date }
+    case "SET_CURRENT_FILM":
+      return { ...state, currentFilm: action.film }
+    case "SET_CURRENT_SHOWING":
+      return { ...state, currentShowing: action.showing }
+    case "SET_SEATS":
+      return { ...state, seats: [...action.seats] };
+    case "SET_FILMS":
+      return { ...state, films: action.films }
+    case "SET_SHOWINGS":
+      return { ...state, showings: action.showings }
+    case "SET_TABLES":
+      return { ...state, tables: [...action.tables] };
+    case "SET_THEATERS":
+      return { ...state, theaters: [...action.theaters] };
+    case "SET_USER":
+      return { ...state, user: action.user };
+    default:
+      return state;
+  }
+}
---
+++ solutions/React with Redux/034 Actions and Reducers 101/src/store/store.js
@@ -0,0 +1,14 @@
+import { createStore } from 'redux';
+import { reducer } from './reducers';
+
+const initialState = {
+  cart: { seats: [], food: [] },
+  currentDate: new Date().setHours(0, 0, 0, 0),
+  films: [],
+  reservations: [],
+  showings: [],
+  theaters: [],
+  user: {},
+};
+
+export const store = createStore(reducer, initialState)
