f7dd67a44cee612275bae4176cd8f3c468ee49fa
rapPayne <bob@creator.net>
2022-03-12 19:58:54 -0600

    Added Hello Redux lab

---
+++ solutions/React with Redux/030 Hello Redux/src/store/store.js
@@ -0,0 +1,15 @@
+import { createStore } from 'redux';
+
+const initialState = {
+  cart: { seats: [], food: [] },
+  currentDate: new Date().setHours(0, 0, 0, 0),
+  films: [],
+  reservations: [],
+  showings: [],
+  theaters: [],
+  user: {},
+};
+
+const reducer = (state, action) => state;
+
+export const store = createStore(reducer, initialState)
